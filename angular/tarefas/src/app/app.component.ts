5cb200bc2839cbed052181c7cbd65689067d00c1
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-08-09 13:50:52 -0300

    FormsModule

--- angular/tarefas/src/app/app.component.ts
+++ angular/tarefas/src/app/app.component.ts
@@ -14,8 +14,12 @@ import { Component } from '@angular/core';
     <img [src]="logo">
     <h1 [class]="'titulo'">Gestão de Tarefas</h1>
     <tarefas></tarefas>
-    <p><input type=text id="nome" name="nome"></p>
-    <p><button (click) = "enviar()">Enviar</button></p>
+
+    <!-- <p><input type=text id="nome" name="nome" #n></p> 
+    <p><button (click) = "enviar(n.value)">Enviar</button></p>-->
+
+    <p><input type=text [(ngModel)]="nome" #n></p>
+    <span *ngIf="n.value.length<3">O nome deve ter no mínimo 3 caracteres.</span>
     <p>Nome: {{nome}}</p>
     <copyright></copyright>
   `
@@ -25,7 +29,8 @@ export class AppComponent {
 
   nome: any = '';
 
-  enviar(): void {
-    // this.nome = <HTMLInputElement>document.getElementById('nome').value;
-  }
+  // enviar(n): void {
+  //   // this.nome = <HTMLInputElement>document.getElementById('nome').value;
+  //   this.nome = n;
+  // }
 }
