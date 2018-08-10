34e0d7201574c889c131d58beca8f48c2ce839be
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-08-09 22:38:45 -0300

    Módulo Tarefa

--- angular/tarefas/src/app/app.component.ts
+++ angular/tarefas/src/app/app.component.ts
@@ -11,26 +11,11 @@ import { Component } from '@angular/core';
     }
   `],
   template: `
-    <img [src]="logo">
     <h1 [class]="'titulo'">Gestão de Tarefas</h1>
-    <tarefas></tarefas>
-
-    <!-- <p><input type=text id="nome" name="nome" #n></p> 
-    <p><button (click) = "enviar(n.value)">Enviar</button></p>-->
-
-    <p><input type=text [(ngModel)]="nome" #n></p>
-    <span *ngIf="n.value.length<3">O nome deve ter no mínimo 3 caracteres.</span>
-    <p>Nome: {{nome}}</p>
+    <tarefa></tarefa>
     <copyright></copyright>
   `
 })
-export class AppComponent { 
-  logo: string = "../assets/logo.png";
-
-  nome: any = '';
-
-  // enviar(n): void {
-  //   // this.nome = <HTMLInputElement>document.getElementById('nome').value;
-  //   this.nome = n;
-  // }
+export class AppComponent {
+  
 }
---
+++ angular/tarefas/src/app/tarefa.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tarefa',
+  styles: [``],
+  template: `
+    <form #f="ngForm" (ngSubmit)="envia(f.value)">
+    <p>
+      <label for="tarefa">Tarefa: </label>
+      <input #tf="ngModel" type="text" [(ngModel)]="t.tarefa" id="tarefa" name="tarefa" required>
+      <span [hidden]="tf.untouched || tf.valid" style="color:red">A tarefa pe obrigatória</span>
+    </p>
+    <p>
+      <label for="prioridade">Prioridade: </label>
+      <select [(ngModel)]="t.prioridade" id="prioridade" name="prioridade" required>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+    </p>
+    <p>
+      <label for="data">Data: </label>
+      <input #td="ngModel" type="date" [(ngModel)]="t.data" id="data" name="data" required>
+      <span [hidden]="td.untouched || td.valid" style="color:red">Data inválida</span>
+    </p>
+    <input type="hidden" [(ngModel)]="preenchedor" name="preenchedor">
+    <button type="submit" [disabled]="!f.valid">Enviar</button>
+  </form>
+  <pre> {{t | json}}</pre>
+  `
+})
+
+export class TarefaComponent { 
+  preenchedor: string = "Yanni";
+
+  t: any = {
+    tarefa: '',
+    prioridade: 1,
+    data: (new Date()).toISOString().slice(0,10)
+  }
+
+  envia(dados): void {
+    console.log(dados)
+  }
+}
